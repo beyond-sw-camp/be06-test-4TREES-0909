54d5fae57c63505792ba6bdd87a2b612a4389925
sue06004 <sue06004@naver.com>
2024-06-05 16:11:42 +0900

    [feat] 공구 전체 조회 API

--- src/main/java/groupbuy/dao/GroupbuyDao.java
+++ src/main/java/groupbuy/dao/GroupbuyDao.java
@@ -1,4 +1,55 @@
 package groupbuy.dao;
 
+import com.zaxxer.hikari.HikariDataSource;
+import config.DataSourceConfig;
+import groupbuy.dto.GroupbuyDto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupbuyDao {
+    HikariDataSource dataSourceConfig;
+
+    public GroupbuyDao() {
+        dataSourceConfig = DataSourceConfig.getInstance();
+    }
+
+    public List<GroupbuyDto> listAll() {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet result = null;
+        List<GroupbuyDto> groupbuyDtos = null;
+        try {
+            connection = dataSourceConfig.getConnection();
+            pstmt = connection.prepareStatement("SELECT gpby_idx, gpby_title, gpby_quantity from GROUP_BUY ");
+            result = pstmt.executeQuery();
+            groupbuyDtos = new ArrayList<>();
+            while (result.next()) {
+                groupbuyDtos.add(new GroupbuyDto(result.getInt("gpby_idx"), result.getString("gpby_title"), result.getInt("gpby_quantity")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                pstmt = null;
+            }
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                connection = null;
+            }
+        }
+        return groupbuyDtos;
+    }
 }
--- src/main/java/groupbuy/servlet/GroupbuyListAllServlet.java
+++ src/main/java/groupbuy/servlet/GroupbuyListAllServlet.java
@@ -1,4 +1,47 @@
 package groupbuy.servlet;
 
-public class GroupbuyListAllServlet {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import config.BaseResponse;
+import config.BaseResponseMessage;
+import groupbuy.dao.GroupbuyDao;
+import groupbuy.dto.GroupbuyDto;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/groupbuy/listall")
+public class GroupbuyListAllServlet extends HttpServlet {
+    GroupbuyDao groupbuyDao;
+    ObjectMapper mapper;
+
+    @Override
+    public void init() throws ServletException {
+        groupbuyDao = new GroupbuyDao();
+        mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<GroupbuyDto> groupbuyDtos = groupbuyDao.listAll();
+        String jsonResponse;
+        BaseResponse response;
+        if (groupbuyDtos != null) {
+            response = new BaseResponse(BaseResponseMessage.GROUPBUY_LIST_SUCCESS, groupbuyDtos);
+        } else {
+            response = new BaseResponse(BaseResponseMessage.GROUPBUY_LIST_FAIL);
+        }
+        jsonResponse = mapper.writeValueAsString(response);
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(jsonResponse);
+
+    }
 }
