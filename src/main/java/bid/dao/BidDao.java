c681659c46ee4c8f73023e765ff449032ed8a0f9
seo-jae-eun <wodms5522@naver.com>
2024-06-05 15:41:54 +0900

    [Feat] 입찰 취소 기능 개발 완료(#19)

--- src/main/java/bid/dao/BidDao.java
+++ src/main/java/bid/dao/BidDao.java
@@ -1,4 +1,49 @@
 package bid.dao;
 
+import com.zaxxer.hikari.HikariDataSource;
+import config.DataSourceConfig;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BidDao {
+    HikariDataSource dataSourceConfig;
+
+    public BidDao() {
+        dataSourceConfig = DataSourceConfig.getInstance();
+    }
+
+    public Boolean delete(int bidIdx) {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        Integer result = null;
+        try {
+            connection = dataSourceConfig.getConnection();
+            pstmt = connection.prepareStatement("UPDATE test.BID SET bid_status='취소', bid_deldate=now() WHERE bid_idx=(?) AND bid_status='등록'");
+            pstmt.setInt(1, bidIdx);
+            result = pstmt.executeUpdate();
+
+            if (result > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+                pstmt = null;
+            }
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException sqlEx) { } // ignore
+                connection = null;
+            }
+        }
+        return false;
+    }
 }
